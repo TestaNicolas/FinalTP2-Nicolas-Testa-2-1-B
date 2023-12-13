eb8d05fad30bcbfcbe47f81a599619b1791f5b3e
NicoTesta <nicolas_testa@outlook.com>
2023-12-13 18:41:16 -0300

    Base v-0.1

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+//import RouterNumeros from './router/numeros.js'
+
+const app = express()
+const PORT = 8080
+
+// APP USE
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// RUTAS
+//app.use('/numeros', new RouterNumeros().OnInit())
+
+// APP LISTEN
+const server = app.listen(PORT, () => console.log(`Servidor escuchado en el puerto ... ${PORT}`))
+server.on('error', error => console.log('Error al levantar el servidor:', error) )
